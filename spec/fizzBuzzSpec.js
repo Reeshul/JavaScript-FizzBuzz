6749609d60e7fb0e7b0edbf175c2465fd56e608a
Reeshul <reeshul@hotmail.com>
2021-02-08 17:14:01 +0000

    completed tests and fizzbuzz

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -24,6 +24,14 @@ describe("FizzBuzz", function () {
     it("returns 'Fizz' when given a number divisible by 3", function() {
       expect(fizzbuzz.convert(3)).toEqual('Fizz');
     })
+    it("returns the same number when given a number not divisble by 3 (or 5)", function() {
+      expect(fizzbuzz.convert(4)).toEqual(4);
+    })
+    it("returns 'Buzz' when given a number divisble by 5 and not 3", function() {
+      expect(fizzbuzz.convert(5)).toEqual('Buzz');
+    })
+    it("returns 'FizzBuzz' when given a number divisible by 15", function() {
+      expect(fizzbuzz.convert(15)).toEqual('FizzBuzz');
+    })
   })
-})
-;
+});
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,5 +1,19 @@
 class FizzBuzz {
-  isDivisibleByThree = n => { return n%3 === 0; }
-  isDivisibleByFive = n => { return n%5 === 0; }
-  convert = n => { return 'Fizz' }
+  isDivisibleByThree = (n) => {
+    return n % 3 === 0;
+  };
+  isDivisibleByFive = (n) => {
+    return n % 5 === 0;
+  };
+  convert = (n) => {
+    if (this.isDivisibleByFive(n) && this.isDivisibleByThree(n)) {
+      return "FizzBuzz";
+    }
+    else if (this.isDivisibleByThree(n)) {
+      return "Fizz";
+    } else if (this.isDivisibleByFive(n)) {
+      return "Buzz";
+    }
+    else return n;
+  };
 }
